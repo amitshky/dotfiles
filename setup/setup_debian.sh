cb4c6336b24f04ecc5d5a89814aa5bdd7d25d55f
amitshky <amitshkymail@gmail.com>
2026-01-21 22:10:11 +0000

    using apt-get instead of apt

--- setup/setup_debian.sh
+++ setup/setup_debian.sh
@@ -1,19 +1,19 @@
 #! /usr/bin/env bash
 
-sudo apt update
-sudo apt upgrade -y
+sudo apt-get update
+sudo apt-get upgrade -y
 
 # install packages
-sudo apt install -y xorg neovim vim git build-essential libx11-dev libxft-dev libxinerama-dev libxcb1-dev libxcb-res0-dev libx11-xcb-dev libxcb-util-dev libxrandr-dev network-manager picom feh arandr pipewire pulseaudio unzip flatpak ntfs-3g wireplumber bluez playerctl brightnessctl tmux rfkill pass gnupg2 clang clang-tools nodejs npm python3-venv python3-pip python-is-python3 ffmpeg 7zip jq poppler-utils fd-find ripgrep fzf zoxide imagemagick pinentry-gtk2 pinentry-tty lua5.1 luarocks adwaita-icon-theme adwaita-icon-theme-legacy breeze-icon-theme lxappearance qt5ct qalc xclip libavcodec-extra breeze gnome-themes-extra btop firefox-esr pcmanfm keepassxc mpv qimgv lazygit gpick darktable copyq flameshot unclutter-xfixes pulsemixer syncthing dunst yt-dlp gallery-dl qbittorrent qalculate-qt okular calcurse libreoffice libreoffice-gtk3 ark krename valgrind mkvtoolnix mkvtoolnix-gui bear dolphin udisks2 kde-cli-tools polkit-kde-agent-1 stow
+sudo apt-get install -y xorg neovim vim git build-essential libx11-dev libxft-dev libxinerama-dev libxcb1-dev libxcb-res0-dev libx11-xcb-dev libxcb-util-dev libxrandr-dev network-manager picom feh arandr pipewire pulseaudio unzip flatpak ntfs-3g wireplumber bluez playerctl brightnessctl tmux rfkill pass gnupg2 clang clang-tools nodejs npm python3-venv python3-pip python-is-python3 ffmpeg 7zip jq poppler-utils fd-find ripgrep fzf zoxide imagemagick pinentry-gtk2 pinentry-tty lua5.1 luarocks adwaita-icon-theme adwaita-icon-theme-legacy breeze-icon-theme lxappearance qt5ct qalc xclip libavcodec-extra breeze gnome-themes-extra btop firefox-esr pcmanfm keepassxc mpv qimgv lazygit gpick darktable copyq flameshot unclutter-xfixes pulsemixer syncthing dunst yt-dlp gallery-dl qbittorrent qalculate-qt okular calcurse libreoffice libreoffice-gtk3 ark krename valgrind mkvtoolnix mkvtoolnix-gui bear dolphin udisks2 kde-cli-tools polkit-kde-agent-1 stow
 
 # i think doing this in the previous command causes conflicts
-sudo apt install -y pipewire-audio-client-libraries
+sudo apt-get install -y pipewire-audio-client-libraries
 
 # install ueberzugpp (debian 13)
 echo 'deb http://download.opensuse.org/repositories/home:/justkidding/Debian_13/ /' | sudo tee /etc/apt/sources.list.d/home:justkidding.list
 curl -fsSL https://download.opensuse.org/repositories/home:justkidding/Debian_13/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_justkidding.gpg > /dev/null
-sudo apt update
-sudo apt install -y ueberzugpp
+sudo apt-get update
+sudo apt-get install -y ueberzugpp
 
 # clone a few repos
 sudo mkdir -p $HOME/{dev,suckless,.fonts,.config,.local,downloads,documents,desktop,music,pictures,videos} /mnt/{hdd,ssd,windows,camera}
@@ -48,7 +48,7 @@ curl -L "https://go.microsoft.com/fwlink/?LinkID=760868" -o /tmp/vscode.deb
 curl -L "http://ftp.uk.debian.org/debian/pool/contrib/t/torbrowser-launcher/torbrowser-launcher_0.3.7-3_amd64.deb" -o /tmp/tor.deb
 sudo dpkg -i /tmp/gcm.deb /tmp/discord.deb
 # using apt to avoid vscode prompt and for some reason tor doesnt get installed using dpkg
-sudo apt install -y /tmp/vscode.deb /tmp/tor.deb
+sudo apt-get install -y /tmp/vscode.deb /tmp/tor.deb
 
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -65,8 +65,8 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 sudo flatpak install -y app/md.obsidian.Obsidian/x86_64/stable com.protonvpn.ww 
 
 # this just freezes dolphin and other apps when they start so remove them
-sudo apt purge -y xdg-desktop-portal*
-sudo apt autoremove -y
+sudo apt-get purge -y xdg-desktop-portal*
+sudo apt-get autoremove -y
 
 # enable network manager service
 sudo systemctl start NetworkManager
