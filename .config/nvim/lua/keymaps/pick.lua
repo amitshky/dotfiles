3627a95fa7431af328a0ae38ba9d321444581f40
amitshky <amitshkymail@gmail.com>
2026-01-24 22:45:16 +0000

    update

--- .config/nvim/lua/keymaps/pick.lua
+++ .config/nvim/lua/keymaps/pick.lua
@@ -1,15 +1,16 @@
-vim.keymap.set("n", "<leader>f", function()
-  require("mini.pick").builtin.cli({
-    command = {
-      "rg",
-      "--files",
-      "--follow",
-      "--color=never",
-      "--hidden",
-      "--glob", "!.git/*",
-      "--glob", "!node_modules/*",
-    },
-  }, { desc = "File picker" })
-end, { desc = "Pick files with rg" })
+vim.keymap.set("n", "<leader>f",
+  function()
+    require("mini.pick").builtin.cli({
+      command = {
+        "rg",
+        "--files",
+        "--follow",
+        "--color=never",
+        "--hidden",
+        "--glob", "!.git/*",
+        "--glob", "!node_modules/*",
+      },
+    })
+  end, { desc = "Pick files with rg" })
 vim.keymap.set('n', '<leader>g', '<cmd>Pick grep_live<cr>', { desc = "Live grep" })
 vim.keymap.set('n', '<leader>B', '<cmd>Pick buffers<cr>', { desc = "Buffers" })
