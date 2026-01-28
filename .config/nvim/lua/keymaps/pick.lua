ca75037aa3681f9b58397e64e9e76a07927cf714
amitshky <amitshkymail@gmail.com>
2026-01-28 22:27:10 +0000

    nvim pick files fix

--- .config/nvim/lua/keymaps/pick.lua
+++ .config/nvim/lua/keymaps/pick.lua
@@ -1,16 +1,3 @@
-vim.keymap.set("n", "<leader>f",
-  function()
-    require("mini.pick").builtin.cli({
-      command = {
-        "rg",
-        "--files",
-        "--follow",
-        "--color=never",
-        "--hidden",
-        "--glob", "!.git/*",
-        "--glob", "!node_modules/*",
-      },
-    })
-  end, { desc = "Pick files with rg" })
+vim.keymap.set("n", "<leader>f", "<cmd>Pick files<cr>", { desc = "Pick files" })
 vim.keymap.set('n', '<leader>g', '<cmd>Pick grep_live<cr>', { desc = "Live grep" })
 vim.keymap.set('n', '<leader>b', '<cmd>Pick buffers<cr>', { desc = "Buffers" })
