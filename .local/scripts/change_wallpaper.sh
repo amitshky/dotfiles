b14e6c1557bb6c5393fba8a4f06214141e0392a6
amitshky <amitshkymail@gmail.com>
2026-06-24 18:01:40 +0100

    update

--- .local/scripts/change_wallpaper.sh
+++ .local/scripts/change_wallpaper.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
-
-DISPLAY=:0 feh --bg-fill --randomize ~/pictures/wallpapers/*.jpg
+#!/bin/sh
+# DISPLAY=:0 feh --no-fehbg --bg-fill --randomize ~/pictures/slideshow/{*.jpg,*.png}
+feh --no-fehbg --bg-fill ~/pictures/slideshow/stellar-blade-blood-rain-evie-0.jpg
