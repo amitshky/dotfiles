f77f56b88b5590304f7724c62fdb1bcacb0c80b4
amitshky <amitshkymail@gmail.com>
2026-06-24 18:16:41 +0100

    update

--- .local/scripts/change_wallpaper.sh
+++ .local/scripts/change_wallpaper.sh
@@ -4,7 +4,7 @@ IMAGE="$1"
 
 # fallback if nothing passed
 if [ -z "$IMAGE" ]; then
-    IMAGE="/home/youruser/pictures/slideshow/stellar-blade-blood-rain-evie-0.jpg"
+    IMAGE="~/pictures/slideshow/stellar-blade-blood-rain-evie-0.jpg"
 fi
 
 # DISPLAY=:0 feh --no-fehbg --bg-fill --randomize ~/pictures/slideshow/{*.jpg,*.png}
